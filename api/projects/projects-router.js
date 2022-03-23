da8c158516480e84f74d355a77c9da0963bd2fcf
Tony Lu <TonyLu@Tonys-iMac.fios-router.home>
2022-03-23 06:57:56 -0400

    get endpoint built: /

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,15 @@
 // Write your "projects" router here!
+const express = require('express');
+const Projects = require('./projects-model');
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+    const projects = await Projects.get();
+    if (!projects) {
+        res.send([]);
+    } else {
+        res.json(projects);
+    }
+})
+
+module.exports = router;
