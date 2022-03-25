a52cbd9d9257267de692df727086111ba00cce21
Tony Lu <TonyLu@Tonys-iMac.fios-router.home>
2022-03-25 01:38:44 -0400

    added custom middleware for action id validation

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,1 +1,18 @@
 // add middlewares here related to actions
+const Actions = require('./actions-model');
+
+async function validateActionId (req, res, next) {
+    const actionFound = await Actions.get(req.params.id);
+    if (!actionFound) {
+        res.status(404).json({
+            message: "action not found"
+        })
+    } else {
+        req.action = actionFound;
+        next();
+    }
+}
+
+module.exports = {
+    validateActionId
+}
