163ba2d87fa1c70d9468035ca7b36425212a4b0d
Tony Lu <TonyLu@Tonys-iMac.fios-router.home>
2022-03-25 01:15:31 -0400

    get endpoint built: /

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,31 @@
 // Write your "actions" router here!
+const express = require('express');
+const Actions = require('./actions-model');
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+    const actions = await Actions.get();
+    if (!actions) {
+        res.send([]);
+    } else {
+        res.json(actions);
+    }
+})
+
+router.get('/:id', (req, res) => {
+    
+})
+
+router.post('/', (req, res) => {
+    
+})
+
+router.put('/:id', (req, res) => {
+    
+})
+
+router.delete('/:id', (req, res) => {
+    
+})
+
+module.exports = router;
