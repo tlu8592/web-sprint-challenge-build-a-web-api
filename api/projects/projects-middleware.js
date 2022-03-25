aea69e93841fbf2c2ca44a9fd98f079b570def44
Tony Lu <TonyLu@Tonys-iMac.fios-router.home>
2022-03-24 23:00:31 -0400

    wrote custom middleware to validate project id

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,17 @@
 // add middlewares here related to projects
+const Projects = require('./projects-model');
+
+async function validateProjectId (req, res, next) {
+    const projectFound = await Projects.get(req.params.id);
+    if (!projectFound) {
+        res.status(404).json({
+            message: "project not found"
+        });
+    } else {
+        next();
+    }
+}
+
+module.exports = {
+    validateProjectId
+}
